c089419b981166bf918fa4bb25befc937d87a320
yakov55994 <141222023+yakov55994@users.noreply.github.com>
2025-08-27 22:30:10 +0300

    update form submit & images design

--- server/server.js
+++ server/server.js
@@ -31,7 +31,33 @@ app.use(express.static('public')); // שם נמצא gallery.html
 
 app.use('/api/images', imagesRoute);
 
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
 
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Redirect the user to the thank you page after a successful submission
+                window.location.href = "https://baloona-store.com/thanks.html";
+            } else {
+                alert('שגיאה בשליחת הטופס. אנא נסה שוב.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('אירעה שגיאה. אנא נסה שוב מאוחר יותר.');
+        });
+    });
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🎈 Gallery server is running on http://localhost:${PORT}`);
